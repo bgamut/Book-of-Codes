5d95936a75d2e982459ef0ba6f4ac274b48781e9
bgamut <bgamut@gmail.com>
2017-07-01 12:25:20 +0900

    update

--- IMAGE2BYTENOPEN.py
+++ IMAGE2BYTENOPEN.py
@@ -5,12 +5,14 @@ from PIL import ImageFilter
 import six
 from array import array
 import io
+import StringIO
 in_file='squwbs.png'
 out_file='myImage.byte'
 with open(in_file, 'rb') as file:
     a=bytearray(file.read())
 with open(out_file,'w') as file:
     file.write(a)
-image1 = Image.open(io.BytesIO(a))
+#image1 = Image.open(io.BytesIO(a))
+image1=Image.open(StringIO.StringIO(a))
 image2=image1.filter(ImageFilter.GaussianBlur(5))
 image2.show()
