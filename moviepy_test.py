ba3ecb5b0feddd7e82f229ed690904cff2acf73e
Bernard Ahn <bgamut@gmail.com>
2017-07-09 11:35:35 +0900

    no message

--- moviepy_test.py
+++ moviepy_test.py
@@ -4,7 +4,7 @@ start=(11.00)
 stop=(12.00)
 clip="IMG_0219.MOV"
 out="test.gif"
-clip1 = (VideoFileClip(clip).subclip((start),(end)).resize(0.5))
+clip1 = (VideoFileClip(clip).subclip((start),(stop)).resize(0.5))
 clip2=moviepy.video.fx.all.time_symmetrize(clip1)
 clip3=clip2.crop(y1=128,y2=952)
 clip3.write_gif(out,fps=15)
