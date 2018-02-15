2b352278508d27b465ce5a5fa036e860bf5a4731
bgamut <bgamut@gmail.com>
2018-02-15 14:22:33 +0900

    Add files via upload

---
+++ fftTest.py
@@ -0,0 +1,102 @@
+import os
+import scipy.io.wavfile as wav
+import numpy as np
+from numpy import convolve
+from numpy.fft import fft,ifft
+fname='/Users/bernardahn/Downloads/source.mp3'
+oname='temp.wav'
+cmd='lame --decode {0} {1}'.format(fname,oname)
+os.system(cmd)
+reference=wav.read(oname)
+
+def split_fft(data):
+    mono=np.arange(len(data[1]))
+    monofft=np.arange(256)
+    leftOnly=np.arange(len(data[1]))
+    rightOnly=np.arange(len(data[1]))
+    sidefft=np.arange(256)
+    resultfftsq=np.zeros(shape=(256,2))
+    for i in data[1]:
+        #a=(data[1][i][0]/2)/max(data[1].max(),data[1].min(),key=abs)
+        a=(data[1][i][0]/2)
+        a.reshape((1,-1))
+        #b = (data[1][i][1]/2)/max(data[1].max(),data[1].min(),key=abs)
+        b = (data[1][i][1]/2)
+        b.reshape((1,-1))
+        mono[i]=a+b
+        leftOnly[i]=a-b
+        rightOnly[i]=b-a
+    monofft=fft(mono,n=256)
+    sidefft=fft(leftOnly,n=256)
+    for i in monofft:
+        a=monofft[i][0]*monofft[i][0]+monofft[i][1]*monofft[i][1]
+        b=sidefft[i][0]*sidefft[i][0]+sidefft[i][1]*sidefft[i][1]
+        resultfftsq[i]=[a,b]
+    print("song analysis complete")
+    return resultfftsq
+
+def deconvolve(data,resultfftsq):
+    mono=np.arange(len(data[1]))
+    monofft=np.arange(256)
+    leftOnly=np.arange(len(data[1]))
+    rightOnly=np.arange(len(data[1]))
+    sidefft=np.arange(256)
+    ir=np.arange(256)
+    for i in data[1]:
+        #a=(data[1][i][0]/2)/max(data[1].max(),data[1].min(),key=abs)
+        a=(data[1][i][0]/2)
+        a.reshape((1,-1))
+        #b = (data[1][i][1]/2)/max(data[1].max(),data[1].min(),key=abs)
+        b = (data[1][i][1]/2)
+        b.reshape((1,-1))
+        mono[i]=a+b
+        leftOnly[i]=a-b
+        rightOnly[i]=b-a
+    monofft=fft(mono,n=256)
+    sidefft=fft(leftOnly,n=256)
+    for i in monofft:
+        a=monofft[i][0]*monofft[i][0]+monofft[i][1]*monofft[i][1]
+        b=sidefft[i][0]*sidefft[i][0]+sidefft[i][1]*sidefft[i][1]
+        c=resultfftsq[0]/a
+        d=resultfftsq[1]/b
+        ir[i]=[a,b]
+    print("done with ir synthesis")
+    return ir
+
+def msconvolve(noise_data,ir):
+    mono=np.arange(len(data[1]))
+    leftOnly=np.arange(len(data[1]))
+    rightOnly=np.arange(len(data[1]))
+    
+    for i in data[1]:
+        #a=(data[1][i][0]/2)/max(data[1].max(),data[1].min(),key=abs)
+        a=(data[1][i][0]/2)
+        a.reshape((1,-1))
+        #b = (data[1][i][1]/2)/max(data[1].max(),data[1].min(),key=abs)
+        b = (data[1][i][1]/2)
+        b.reshape((1,-1))
+        mono[i]=a+b
+        leftOnly[i]=a-b
+        rightOnly[i]=b-a
+    mono_convolve=convolve(mono,ir[0])/2
+    left_convolve=convolve(leftOnly,ir[1])/2
+    right_convolve=convolve(rightOnly,ir[1])/2
+    result=np.zeros(shape=(2,len(mono_convolve)))
+    for i in mono_convolve:
+        result[i]=[left_convolve[i]+mono_convolve[i],right_convolve[i]+mono_convolve[i]]
+    return result
+
+
+red=wav.read("noise RED.wav")
+print("red : "+str(len(red[1]))+" samples.")
+brown=wav.read("noise BROWN.wav")
+print("brown : "+str(len(brown[1]))+" samples.")
+white=wav.read("noise WHITE.wav")
+print("white : "+str(white[1])+" samples.")
+redir=msconvolve(deconvolve(red,split_fft(reference)))
+brownir=msconvolve(deconvolve(brown,split_fft(reference)))
+whiteir=msconvolve(deconvovle(white,split_fft(reference)))
+
+wav.write("redir.wav",44100,redir)
+wav.write("brownir.wav",44100,brownir)
+wav.write("whiteir.wav",44100,whiteir)
