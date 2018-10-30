73b7291323e89bdbbb1c257775c9e43da24c62cd
bgamut <bgamut@gmail.com>
2018-10-30 14:10:50 +0900

    Update filterarray.py

--- filterarray.py
+++ filterarray.py
@@ -1,86 +1,5 @@
 import math
 
-def filter(buffer,mode="HP48",sr=44100,freq=175,resonance=0):
-    new_buffer=[]
-    for i in range(len(buffer)):
-        new_buffer.append(0)
-    cutoff=2*math.sin(math.pi*(freq/sr))
-    feedbackAmount = resonance + resonance/(1.0-cutoff)
-    buf0 = 0
-    buf1 = 0
-    buf2 = 0
-    buf3 = 0
-    buf4 = 0
-    buf5 = 0
-    buf6 = 0
-    buf7 = 0
-    buf8 = 0
-    buf9 = 0
-    buf10 = 0
-    buf11 = 0
-    buf12 = 0
-    buf13 = 0
-    buf14 = 0
-    buf15 = 0
-    if (mode == "HP12"):
-        for i in range(len(buffer)):
-            """
-            buf0 = buf0+cutoff*(buffer[i]*buf0)
-            """
-            buf0 += cutoff * (buffer[i] - buf0)
-            buf1 += cutoff * (buf0 - buf1)
-            new_buffer[i]=buffer[i]-buf1
-        
-    elif (mode =="LP12"):
-        for i in range(len(buffer)):
-            
-            buf0 = buf0+cutoff*(buffer[i]*buf0)
-            buf1 = buf1+cutoff*(buf0-buf1)
-            new_buffer[i]=buf1
-    elif (mode == "HP48"):
-        print('in HP48')
-        for i in range(len(buffer)):
-            buf0 += cutoff * (buffer[i] - buf0)
-            buf1 += cutoff * (buf0 - buf1)
-            buf2 += cutoff * (buf1 - buf2)
-            buf3 += cutoff * (buf2 - buf3)
-            buf4 += cutoff * (buf3 - buf4)
-            buf5 += cutoff * (buf4 - buf5)
-            buf6 += cutoff * (buf5 - buf6)
-            buf7 += cutoff * (buf6 - buf7)
-            new_buffer[i]=buffer[i] - buf7
-    elif(mode=="LP48"):
-        for i in range(len(buffer)):
-            buf0 += cutoff * (buffer[i] - buf0)
-            buf1 += cutoff * (buf0 - buf1)
-            buf2 += cutoff * (buf1 - buf2)
-            buf3 += cutoff * (buf2 - buf3)
-            buf4 += cutoff * (buf3 - buf4)
-            buf5 += cutoff * (buf4 - buf5)
-            buf6 += cutoff * (buf5 - buf6)
-            buf7 += cutoff * (buf6 - buf7)
-            new_buffer[i]=buf7
-    elif(mode=="LP96"):
-        for i in range(len(buffer)):
-            buf0 += cutoff * (buffer[i] - buf0)
-            buf1 += cutoff * (buf0 - buf1)
-            buf2 += cutoff * (buf1 - buf2)
-            buf3 += cutoff * (buf2 - buf3)
-            buf4 += cutoff * (buf3 - buf4)
-            buf5 += cutoff * (buf4 - buf5)
-            buf6 += cutoff * (buf5 - buf6)
-            buf7 += cutoff * (buf6 - buf7)
-            buf8 += cutoff * (buf7 - buf8)
-            buf9 += cutoff * (buf8 - buf9)
-            buf10 += cutoff * (buf9 - buf10)
-            buf11 += cutoff * (buf10 - buf11)
-            buf12 += cutoff * (buf11 - buf12)
-            buf13 += cutoff * (buf12 - buf13)
-            buf14 += cutoff * (buf13 - buf14)
-            buf15 += cutoff * (buf14 - buf15)
-           
-            new_buffer[i]=buf15
-    return new_buffer
 
 def read_wav_file(input_file_path,output_file_path):
     from scipy.io.wavfile import read
@@ -106,6 +25,7 @@ import os
 import wx
 import wx.lib.agw.multidirdialog as MDD
 from scipy.io.wavfile import read
+from scipy.io.wavfile import write
 import numpy as np 
 import math
 wildcard = "Python source (*.py) |*.py|" \
@@ -118,21 +38,26 @@ class MyForm(wx.Frame):
         self.barkscale = [51,127,200,270,370,440,530,640,770,950,1200,1550,19500]
         self.referencepath=[]
         self.originalpath=[]
-        self.averages=[]
-        self.standard_deviations=[]
+        self.mid_averages=[]
+        self.mid_standard_deviations=[]
+        self.side_averages=[]
+        self.side_standard_deviations=[]
         for i in range(len(self.barkscale)+1):
             self.averages.append(0)
             self.standard_deviations.append(0)
         wx.Frame.__init__(self, None, wx.ID_ANY, "Mastering")
         panel = wx.Panel(self, wx.ID_ANY)
         self.currentDirectory= os.getcwd()
-        openFileDlgBtnOne = wx.Button(panel, label = "Show OPEN Reference")
+        openFileDlgBtnOne = wx.Button(panel, label = "OPEN Reference")
         openFileDlgBtnOne.Bind(wx.EVT_BUTTON,self.onOpenReference)
-        openFileDlgBtnTwo = wx.Button(panel, label = "Show OPEN Original")
+        openFileDlgBtnTwo = wx.Button(panel, label = "OPEN Original")
         openFileDlgBtnTwo.Bind(wx.EVT_BUTTON,self.onOpenOriginal)
+        runFileDlgBtn = wx.Button(panel, label = 'Run Mastering')
+        runFileDlgBtn.Bind(wx.EVT_BUTTON,self.mastering)
         sizer = wx.BoxSizer(wx.VERTICAL)
         sizer.Add(openFileDlgBtnOne, 0,wx.ALL|wx.CENTER,5)
         sizer.Add(openFileDlgBtnTwo, 0,wx.ALL|wx.CENTER,5)
+        sizer.Add(runFileDlgBtn, 0,wx.ALL|wx.CENTER,5)
         panel.SetSizer(sizer)
     def analysebuffer(self,buffer,sr):
         def LP48(buffer,freq,sr):
@@ -180,22 +105,99 @@ class MyForm(wx.Frame):
                 new_buffer.append(buffer[i] - buf7)
             return new_buffer
         obj=[]
-        
+        av=[]
+        st=[]
+        for i in range(len(self.barkscale)+1):
+            av.append(0)
+            st.append(0)
         for i in range(len(buffer)):
-            self.averages[0]+=abs(LP48(buffer,51,sr)[i])/len(buffer)
-            self.averages[-1]+=abs((HP48(buffer,19500,sr)[i])/len(buffer))
-        for i in range(len(self.barkscale-1)):
+            av[0]+=abs(LP48(buffer,51,sr)[i])/len(buffer)
+            av[-1]+=abs((HP48(buffer,19500,sr)[i])/len(buffer))
+        for i in range(len(self.barkscale)-1):
             for j in range(len(buffer)):
-                self.averages[i+1]+=abs(LP48(HP48(buffer,barkscale[i],sr),barkscale[i]+1,sr)[j])/len(buffer)
+                av[i+1]+=abs(LP48(HP48(buffer,barkscale[i],sr),barkscale[i]+1,sr)[j])/len(buffer)
         for i in range(len(buffer)):
-            self.standard_deviations[0]+=(abs(LP48(buffer,51,sr)[i])-self.averages[0])/len(buffer)
-            self.standard_deviations[-1]+=(abs((HP48(buffer,19500,sr)[i])-self.averages[-1])/len(buffer))
-        for i in range(len(self.barkscale-1)):
+            st[0]+=(abs(LP48(buffer,51,sr)[i])-av[0])/len(buffer)
+            st[-1]+=(abs((HP48(buffer,19500,sr)[i])-av[-1])/len(buffer))
+        for i in range(len(self.barkscale)-1):
             for j in range(len(buffer)):
-                self.standard_deviations[i+1]+=abs(LP48(HP48(buffer,barkscale[i],sr),barkscale[i]+1,sr)[j]-self.averages[i+1])/len(buffer)
-        obj.append(self.averages)
-        obj.append(self.standard_deviations)
+                st[i+1]+=abs(LP48(HP48(buffer,barkscale[i],sr),barkscale[i]+1,sr)[j]-av[i+1])/len(buffer)
+        obj.append(av)
+        obj.append(st)
         return obj
+    def produceBuffer(self,originalaverage,referenceaverage,standardRatio,originalbuffer,originalsr):
+        def LP48(buffer,freq,sr):
+            buf0 = 0
+            buf1 = 0
+            buf2 = 0
+            buf3 = 0
+            buf4 = 0
+            buf5 = 0
+            buf6 = 0
+            buf7 = 0
+            cutoff=2*math.sin(math.pi*(freq/sr))
+            new_buffer=[]
+            for i in range(len(buffer)):
+                buf0 += cutoff * (buffer[i] - buf0)
+                buf1 += cutoff * (buf0 - buf1)
+                buf2 += cutoff * (buf1 - buf2)
+                buf3 += cutoff * (buf2 - buf3)
+                buf4 += cutoff * (buf3 - buf4)
+                buf5 += cutoff * (buf4 - buf5)
+                buf6 += cutoff * (buf5 - buf6)
+                buf7 += cutoff * (buf6 - buf7)
+                new_buffer.append(buf7)
+            return new_buffer
+        def HP48(buffer,freq,sr):
+            buf0 = 0
+            buf1 = 0
+            buf2 = 0
+            buf3 = 0
+            buf4 = 0
+            buf5 = 0
+            buf6 = 0
+            buf7 = 0
+            cutoff=2*math.sin(math.pi*(freq/sr))
+            new_buffer=[]
+            for i in range(len(buffer)):
+                buf0 += cutoff * (buffer[i] - buf0)
+                buf1 += cutoff * (buf0 - buf1)
+                buf2 += cutoff * (buf1 - buf2)
+                buf3 += cutoff * (buf2 - buf3)
+                buf4 += cutoff * (buf3 - buf4)
+                buf5 += cutoff * (buf4 - buf5)
+                buf6 += cutoff * (buf5 - buf6)
+                buf7 += cutoff * (buf6 - buf7)
+                new_buffer.append(buffer[i] - buf7)
+            return new_buffer
+        obj=[]
+        temp = []
+        for i in range(len(originalbuffer)):
+            new_buffer=LP48(originalbuffer,barkscale[0],originalsr)[i]
+            if (originalbuffer[i]>0):
+                temp.append((new_buffer[i]-originalaverage[0])*standardRatio[0]+referenceaverage[0])
+            elif (originalbuffer[i]<0):
+                temp.append((new_buffer[i]+originalaverage[0])*standardRatio[0]-referenceaverage[0])
+            elif (originalbuffer[i]==0):
+                temp.append(0)
+        for i in range(len(self.barkscale)-1):
+            new_buffer = LP48(HP48(originalbuffer,barkscale[i],originalsr),barkscale[i+1],originalsr)
+            for j in range(len(originalbuffer)):
+                if (new_buffer[i]>0):
+                    temp.append((new_buffer[i]-originalaverage[i+1])*standardRatio[i+1]+referenceaverage[i+1])
+                elif (new_buffer[i]<0):
+                    temp.append((new_buffer[i]+originalaverage[i+1])*standardRatio[i+1]-referenceaverage[i+1])
+                elif (new_buffer[i]==0):
+                    temp.append(0)
+        for i in range(len(originalbuffer)):
+            new_buffer=HP48(originalbuffer,barkscale[0],originalsr)[i]
+            if (new_buffer[i]>0):
+                temp[i]+=((new_buffer[i]-originalaverage[-1])*standardRatio[-1]+referenceaverage[-1])
+            elif (new_buffer[i]<0):
+                temp+=((new_buffer[i]+originalaverage[-1])*standardRatio[-1]-referenceaverage[-1])
+            elif (new_buffer[i]==0):
+                temp+=(0)  
+        return temp
 
 
 
@@ -216,8 +218,9 @@ class MyForm(wx.Frame):
                 self.originalpath.append(path)
             for i in range(len(self.originalpath)):
                 print (self.originalpath[i])
-                """
-                a=read(path)        
+                a=read(self.originalpath[i])
+                print(a)
+                """        
                 b=[]
                 for i in range(len(a[1])):
                     b.append(a[1][i][0])
@@ -243,7 +246,84 @@ class MyForm(wx.Frame):
             for i in range(len(self.referencepath)):
                 print (self.referencepath[i])
 
-    def 
+    def mastering(self):
+        for file in self.referencepath:
+            a=read(file)
+            left = []
+            right = []
+            mid = []
+            side = []
+            try:
+                divideby = (np.iinfo(a[1]).min)*(-1)
+            except:
+                divideby = 1
+            if (type(a[1]) is list):
+                for i in range(len(a[1])):
+                    left.append(a[1][i][0]/divideby)
+                    right.append(a[1][i][1]/divideby)
+                for i in range(len(left)):
+                    mid.append((left[i]+right[i])/2.0)
+                    side.append(left[i]-mid[i])
+            elif (type(a[1]) is not int):
+                for i in range(len(a[1])):
+                    mid.append(a[1][i])
+                    side.append(0)
+            temp_mid=self.analysebuffer(mid, a[0])
+            temp_side=self.analysebuffer(side, a[0])
+            for i in range(len(self.referencepath)):
+                self.mid_averages+=temp_mid[0][i]/len(self.referancepath)
+                self.mid_standard_deviations+=temp_mid[1][i]/len(self.referancepath)
+                self.side_averages+=temp_mid[0][i]/len(self.referancepath)
+                self.side_standard_deviations+=temp_mid[1][i]/len(self.referancepath)
+        for file in self.originalpath:
+            a=read(file)
+            left = []
+            right = []
+            mid = []
+            side = []
+            mid_average_ratio=[]
+            mid_standard_ratio=[]
+            side_average_ratio=[]
+            side_standard_ratio=[]
+            obj=[]
+            if (type(a[1]) is list):
+                for i in range(len(a[1])):
+                    left.append(a[1][i][0])
+                    right.append(a[1][i][1])
+                for i in range(len(left)):
+                    mid.append((left[i]+right[i])/2.0)
+                    side.append(left[i]-mid[i])
+            elif (type(a[1]) is not int):
+                for i in range(len(a[1])):
+                    mid.append(a[1][i])
+                    side.append(0)
+            temp_mid=self.analysebuffer(mid, a[0])
+            temp_side=self.analysebuffer(side, a[0])
+            
+            mid_averages=temp_mid[0]
+            mid_standard_deviations=temp_mid[1]
+            side_averages=temp_mid[0]
+            side_standard_deviations=temp_mid[1]
+            for i in range(len(self.mid_averages)):
+                mid_average_ratio.append(self.mid_averages[i]/mid_averages[i])
+                mid_standard_ratio.append(self.mid_standard_deviations[i]/mid_standard_deviations[i])
+                side_average_ratio.append(self.side_averages[i]/side_averages[i])
+                side_standard_ratio.append(self.side_standard_deviation[i]/side_standard_deviations[i])
+            obj.append(mid_average_ratio)
+            obj.append(mid_standard_ratio)
+            obj.append(side_average_ratio)
+            obj.append(side_standard_ratio)
+            mid=self.producebuffer(mid_averages,self.mid_averages,mid_standard_ratio,temp_mid,a[0])
+            side=self.producebuffer(side_averages,self.side_averages,side_standard_ratio,temp_side,a[0])
+            master = np.zeros((len(mid),2),'int16')
+            for i in range(len(mid)):
+                master[i][0]=(mid[i]+side[i])*np.iinfo('int16').max
+                master[i][1]=(mid[i]-side[i])*np.iinfo('int16').max
+            master = np.zeros((len(mid),2),'int16')
+            #need to iterate through file names and foldername for the code below
+            write('master.wav',a[0],master)
+
+
 if __name__ == "__main__":
     app = wx.App(False)
     frame = MyForm()
